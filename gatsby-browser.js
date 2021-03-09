06dba5a75ba336d02493dad2129e03e922538993
Pea <pea@misfist.com>
2021-03-09 00:18:14 -0500

    Import webfonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,12 @@
 // custom typefaces
 import "@fontsource/montserrat"
+import "@fontsource/montserrat/700.css"
+import "@fontsource/montserrat/900.css"
 import "@fontsource/poppins"
+import "@fontsource/poppins/700.css"
+import "@fontsource/poppins/900.css"
 import "@fontsource/work-sans"
+import "@fontsource/work-sans/700.css"
 // import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/assets/normalize.css"
