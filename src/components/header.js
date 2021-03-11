38ae8d11af125267bfdbed2518de2d056d14c8df
Pea <pea@misfist.com>
2021-03-10 21:09:01 -0500

    Add class to body when nav is stuck

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useStaticQuery, graphql } from "gatsby"
-import parse from "html-react-parser"
-import MainMenu from "./menu"
+import { useStaticQuery, graphql } from 'gatsby'
+import parse from 'html-react-parser'
+import Helmet from 'react-helmet'
+import MainMenu from './menu'
 import SocialMenu from './menu-social'
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
 
 const Header = ( { data } ) => {
   const [isSticky, setSticky] = useState( false );
@@ -10,9 +13,10 @@ const Header = ( { data } ) => {
 
   const handleScroll = () => {
     const offset = document.querySelector( '.main-heading' ).offsetHeight;
+
     if ( ref.current ) {
       setSticky( ref.current.getBoundingClientRect().top <= -offset );
-    }
+    } 
   };
 
   useEffect(() => {
@@ -48,26 +52,33 @@ const Header = ( { data } ) => {
   let backgroundImage = customHeaders.length ? customHeaders[Math.floor(Math.random() * customHeaders.length)] : '';
 
   const style = {
-    backgroundImage:  'linear-gradient(rgba(11, 79, 108, 0.45), rgba(11, 79, 108, 0.45)), url(' + backgroundImage + ')',
+    backgroundImage:  'linear-gradient(rgba(11, 79, 108, 0.75), rgba(11, 79, 108, 0.75)), url(' + backgroundImage + ')',
     WebkitTransition: 'all', // note the capital 'W' here
     msTransition: 'all' // 'ms' is the only lowercase vendor prefix
   };
 
   return (
-    <header className={`global-header lazy sticky-wrapper${isSticky ? ' sticky' : ''}`} style={style} loading="lazy" ref={ref}>
-      <div className="main-heading">
-        <h1 className="site-title">
-          <a href={process.env.WEBSITE_URL}>{parse(title)}</a>
-        </h1>
-        <div
-          className="site-description"
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></div>
-        <SocialMenu />
-      </div>
+    <>
+      <Helmet
+        bodyAttributes={{
+            class: isSticky ? 'is-stuck' : ''
+        }}
+      />
+      <header className={`global-header lazy sticky-wrapper${isSticky ? ' sticky' : ''}`} style={style} loading="lazy" ref={ref}>
+        <div className="main-heading">
+          <h1 className="site-title">
+            <a href={process.env.WEBSITE_URL}>{parse(title)}</a>
+          </h1>
+          <div
+            className="site-description"
+            dangerouslySetInnerHTML={{ __html: description }}
+          ></div>
+          <SocialMenu />
+        </div>
 
-      <MainMenu />
-    </header>
+        <MainMenu />
+      </header>
+    </>
   )
 }
 
