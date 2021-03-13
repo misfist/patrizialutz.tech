22bd614b065d30a19a2adc0fcf38979ba05d9b90
Pea <pea@misfist.com>
2021-03-13 01:46:14 -0500

    Add print button

--- src/components/menu-social.js
+++ src/components/menu-social.js
@@ -4,11 +4,13 @@
  *
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
-import React from "react"
+import React, { useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import MenuItem from "./menu-item"
+import { SVG } from '../utils/sprites'
 
 const SocialMenu = () => {
+
   const data = useStaticQuery(graphql`
     {
       wpMenu(slug: {eq: "social-menu"}) {
@@ -34,23 +36,42 @@ const SocialMenu = () => {
 
   const menuItems = menu.menuItems.nodes
 
+  if( !menuItems ) {
+    return null;
+  }
+
+  const handleClick = ( event ) => {
+    window.print();
+  }
+
   return (
     <nav id={menu.slug} className={menu.slug}>
-        {menuItems && 
-          <ul id="primary-menu" className="menu">
-            {menuItems.map( ( menuItem, index ) => {
-              return (
-                <li
-                  key={menuItem.databaseId}
-                  id={`menu-item-${menuItem.databaseId}`}
-                  className={`menu-item ${menuItem.cssClasses}`}
-                >
-                <MenuItem menuItem={menuItem} />
-              </li>
-              )
-            } )}
-          </ul>
-        }
+      <ul id="primary-menu" className="menu">
+        {menuItems.map( ( menuItem, index ) => {
+          return (
+            <li
+              key={menuItem.databaseId}
+              id={`menu-item-${menuItem.databaseId}`}
+              className={`menu-item ${menuItem.cssClasses}`}
+            >
+            <MenuItem menuItem={menuItem} />
+          </li>
+          )
+        } )}
+        <li className="menu-item print">
+          <div 
+            className="js-click" 
+            onClick={ handleClick }
+          >
+            <SVG 
+              id={`print`}
+              width='32'
+              height='32'
+            />
+            <span className="menu-label">Print</span>
+          </div>
+        </li>
+      </ul>
     </nav>
   )
 }
